b06e9e8e993f0152deb946558f090bca4ac4a0c7
Rafay <arafaymbir@gmail.com>
2020-08-09 00:35:07 -0400

    added tests to class + Axios testing

--- __tests__/translator.test.js
+++ __tests__/translator.test.js
@@ -1,17 +1,33 @@
-const {translate} = require('../js/translator')
-const {bodyMassIndex} = require('../js/translator')
-const {poundsToKg} = require('../js/translator')
+import {Formulas} from '../js/translator'
+import {myCall} from '../js/translator'
+const method = new Formulas()
+const unsplash = new myCall()
+import mockAxios from 'axios'
+
 
 test('reverses a string', () => {
-    expect(translate("yafar")).toBe("rafay")
+    expect(method.translate("yafar")).toBe("rafay")
 
 })
 
 test('returns BMI', () =>{
-    expect(bodyMassIndex(71, 1.80)).toBe(21.91358024691358)
+    expect(method.bodyMassIndex(71, 1.80)).toBe(21.91358024691358)
 })
 
 test('returns weight in KG', () => {
-    expect(poundsToKg(1)).toBe(0.4535147392290249)
+    expect(method.poundsToKg(1)).toBe(0.4535147392290249)
 
+})
+
+
+it("calls axios and returns images", async () => {
+    mockAxios.get.mockImplementationOnce(() => 
+    Promise.resolve({
+        data: {
+            results : ["cute.jpg"]
+        }
+    }))
+
+    const images = await unsplash.myfunc("kittens")
+    expect(images).toEqual(["cute.jpg"])
 })
--- js/translator.js
+++ js/translator.js
@@ -1,17 +1,57 @@
-exports.translate = (string) => {
-    return string.split("").reverse().join("");
+const axios = require('axios').default;
+
+class Formulas{
+    constructor(){
+
+    }
+    translate(string) {
+        return string.split("").reverse().join("");
+    }
+    
+    
+    
+    
+    bodyMassIndex(weight, height) {
+        height = height * height
+        return weight / height
+    }
+    
+    poundsToKg(pounds){
+        return pounds / 2.205
+    
+    }
+}
+
+
+class myCall{
+    constructor(){
+        
+    }
+   async myfunc() {
+        const response = await axios.get("https://api.unsplash.com/search/photos",
+        {
+            params : {
+                client_id: process.env.APP_SECRET_KEY
+            }
+        
+        }
+        
+        )
+        return response.data.results;
+        }
+
 }
 
 
 
 
-exports.bodyMassIndex = (weight, height) => {
-    height = height * height
-    return weight / height
-}
-
-exports.poundsToKg = (pounds) =>{
-    return pounds / 2.205
+module.exports = {
+    Formulas,
+    myCall
+    
 
 }
+
+
+
 
